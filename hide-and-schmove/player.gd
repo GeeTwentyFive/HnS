0f7489ac10a26161472354a6b6a26b91613c98a3
Gee <alickoamarko@gmail.com>
2026-01-14 00:13:17 +0100

    Added back WASD air control, for just seeker

--- hide-and-schmove/player.gd
+++ hide-and-schmove/player.gd
@@ -164,7 +164,7 @@ func _physics_process(_delta: float) -> void:
 	if movement_direction:
 		movement_direction = movement_direction.rotated(yaw)
 		
-		if is_on_ground:
+		if is_on_ground or is_seeker:
 			apply_central_force(
 				Vector3(movement_direction.x, 0.0, movement_direction.y) *
 				move_speed
